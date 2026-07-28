3a69519d5d0557282203fe7d1b45477d7b604769
cver <cver@163.com>
2026-07-28 21:54:01 +0800

    init

--- 1.py
+++ 1.py
@@ -2,6 +2,4 @@ import torch
 from torch_cluster import radius_graph
 print(radius_graph.__file__)  # 看路径里有没有 "cuda" 或 "cpu"
 
-# 安装torch2.8版本才行，和2.11
-# kaolin torch-cluster
-# flashatten也装上
+# 安装torch_geometric
